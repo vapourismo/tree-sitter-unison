af49bdc43915bbeb25ce843ac27443dfd79c319e
Ole Krüger <ole@vprsm.de>
2023-02-27 18:26:04 +0000

    Dev environment

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          nil
+          alejandra
+          tree-sitter
+          nodejs
+          nodePackages.typescript-language-server
+        ];
+      };
+    });
+}
